c1ac97ade89c42bae21dbdac9fbbc6103cbc166d
Natasha da Silva Fernandes <a25natashadsf@teis.intranet>
2025-09-11 19:58:26 +0200

    Atividade 3

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int saldo = 56;
+        boolean habilitado = true;
+
+            if (saldo >= 1) {
+                System.out.printf("Habilitada/o.");
+            } else{
+            System.out.printf("No Habilitada/o.");
+            }
+    }
+}
